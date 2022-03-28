0ed21eaca7c9ef8d6b729e201ceb4495515af007
Thales Castro <thalesccastro@gmail.com>
2022-03-27 21:28:10 -0300

    refactor: store method in LogController

--- app/Http/Controllers/Api/LogController.php
+++ app/Http/Controllers/Api/LogController.php
@@ -9,55 +9,54 @@ use Illuminate\Support\Facades\Storage;
 
 class LogController extends Controller
 {
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $text = Storage::disk('logs')->get('logs.txt');
+        try {
+            $text = Storage::disk('logs')->get('logs.txt');
+        } catch (\Exception $e) {
+            return response()->json(['500' => 'Internal Server Error']);
+        }
 
         $textLines = preg_split('/\r\n|\r|\n/', $text);
 
-        $offset = 99900;
-        $length = 100;
+        if ($request->offset && $request->length && ($request->offset + $request->length) > 10000) {
+            return response()->json(['400' => 'Bad Request']);
+        }
+
+        $offset = $request->offset ? $request->offset : 0;
+        $length = $request->length ? $request->length : 100;
 
         $textToProcess = array_slice($textLines, $offset, $length);
 
         foreach ($textToProcess as $jsonLine) {
+            $entry = [];
             $line = json_decode($jsonLine);
 
-            $requestHeaderId = $this->formatObjectAndSave('request_headers', $line->request->headers);
-            $requestId = $this->formatObjectAndSave('requests', $line->request, ['request_header_id' => $requestHeaderId]);
+            try {
+                $requestHeaderId = $this->formatObjectAndSave('request_headers', $line->request->headers);
+                $responseHeaderId = $this->formatObjectAndSave('response_headers', $line->response->headers);
 
-            $this->insertPivot($line->request->querystring, 'querystrings', 'description', 'querystring_request', 'querystring_id', 'request_id', $requestId);
+                $entryForeignKeys['service_id']  = $this->formatObjectAndSave('services', $line->service);
+                $entryForeignKeys['latency_id'] = $this->formatObjectAndSave('latencies', $line->latencies);
+                $entryForeignKeys['authenticated_entity_id'] = $this->formatObjectAndSave('authenticated_entities', $line->authenticated_entity->consumer_id);
 
-            $responseHeaderId = $this->formatObjectAndSave('response_headers', $line->response->headers);
-            $responseId = $this->formatObjectAndSave('responses', $line->response, ['response_header_id' => $responseHeaderId]);
+                $entryForeignKeys['request_id'] = $this->formatObjectAndSave('requests', $line->request, ['request_header_id' => $requestHeaderId]);
+                $entryForeignKeys['response_id'] = $this->formatObjectAndSave('responses', $line->response, ['response_header_id' => $responseHeaderId]);
+                $entryForeignKeys['route_id'] = $this->formatObjectAndSave('routes', $line->route, ['service_id' => $entryForeignKeys['service_id']]);
 
-            $authenticatedEntityId = $this->formatObjectAndSave('authenticated_entities', $line->authenticated_entity->consumer_id);
+                $this->insertPivot($line->request->querystring, 'querystrings', 'querystring_request', 'querystring_id', 'request_id', $entryForeignKeys['request_id']);
+                $this->insertPivot($line->route->methods, 'methods', 'method_route', 'method_id', 'route_id', $entryForeignKeys['route_id']);
+                $this->insertPivot($line->route->paths, 'paths', 'path_route', 'path_id', 'route_id', $entryForeignKeys['route_id']);
+                $this->insertPivot($line->route->protocols, 'protocols', 'protocol_route', 'protocol_id', 'route_id', $entryForeignKeys['route_id']);
 
-            $serviceId = $this->formatObjectAndSave('services', $line->service);
+                $this->formatObjectAndSave('entries', $line, $entryForeignKeys);
 
-            $routeId = $this->formatObjectAndSave('routes', $line->route, ['service_id' => $serviceId]);
-
-            $this->insertPivot($line->route->methods, 'methods', 'description', 'method_route', 'method_id', 'route_id', $routeId);
-            $this->insertPivot($line->route->paths, 'paths', 'description', 'path_route', 'path_id', 'route_id', $routeId);
-            $this->insertPivot($line->route->protocols, 'protocols', 'description', 'protocol_route', 'protocol_id', 'route_id', $routeId);
-
-            $latencyId = $this->formatObjectAndSave('latencies', $line->latencies);
-
-            $this->formatObjectAndSave('entries', $line, [
-                'request_id' => $requestId,
-                'response_id' => $responseId,
-                'authenticated_entity_id' => $authenticatedEntityId,
-                'service_id' => $serviceId,
-                'route_id' => $routeId,
-                'latency_id' => $latencyId,
-            ]);
+            } catch (\Exception $e) {
+                return response()->json(['500' => 'Internal Server Error']);
+            }
         }
+
+        return response()->json(['200' => 'OK']);
     }
 
     private function formatObjectAndSave($tableName, $dataObject, $foreignKeys = null)
@@ -89,12 +88,12 @@ class LogController extends Controller
         return $this->insert($tableName, $formattedArray);
     }
 
-    private function insertPivot($dataObject, $otherTableName, $otherTableField, $pivotTableName, $otherTableKey, $thisTableKey, $thisTableId)
+    private function insertPivot($dataObject, $otherTableName, $pivotTableName, $otherTableKey, $thisTableKey, $thisTableId)
     {
         $dataArray = (array) $dataObject;
         $arrayLength = count($dataArray);
         for ($i = 0; $i < $arrayLength; $i++) {
-            $otherTableId = $this->findOrInsert($otherTableName, $otherTableField, $i, $dataArray);
+            $otherTableId = $this->findOrInsert($otherTableName, 'description', $i, $dataArray);
             $this->insert($pivotTableName, [
                 $otherTableKey => $otherTableId,
                 $thisTableKey => $thisTableId,
